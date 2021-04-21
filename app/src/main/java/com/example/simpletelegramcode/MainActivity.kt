0ac3b3247ab919f4f3efdc987a9687937ce7f98b
m.varvaei <m@123456>
2021-04-22 03:43:46 +0430

    testing fileLog class: playing video and logging too. there is no lag

--- app/src/main/java/com/example/simpletelegramcode/MainActivity.kt
+++ app/src/main/java/com/example/simpletelegramcode/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.simpletelegramcode
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.SurfaceHolder
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.simpletelegramcode.utils.FileLog
 import com.example.simpletelegramcode.utils.SafeAndroidUtilities
 import com.example.simpletelegramcode.utils.getMemoryPermission
@@ -13,8 +17,8 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var frameLayout:FrameLayout
-
+    lateinit var frameLayout: FrameLayout
+    lateinit var fileLoggerTimer: Timer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         frameLayout = FrameLayout(this)
@@ -25,12 +29,6 @@ class MainActivity : AppCompatActivity() {
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
         )
-        val simpleButton = Button(this)
-        simpleButton.text = "my father my fathermy father "
-        simpleButton.text =
-            SafeAndroidUtilities.ellipsizeCenterEnd(simpleButton,
-                "my father",100,10)
-        frameLayout.addView(simpleButton)
         if (hasMemoryPermission()) {
             testFileLogger()
         } else {
@@ -38,13 +36,59 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun testFileLogger(){
+    private fun testFileLogger() {
+        addVideoView()
         FileLog.ensureInitied()
-        val timer = Timer()
-        timer.schedule(object :TimerTask(){
+        var ramTestNumber = 0
+        fileLoggerTimer = Timer()
+        fileLoggerTimer.schedule(object : TimerTask() {
             override fun run() {
-                FileLog.e("this is test log")
+                ramTestNumber++
+                FileLog.e("testing funny Video $ramTestNumber")
             }
-        },2000,500)
+        }, 1000, 1)
+    }
+
+    private fun addVideoView() {
+        val videoPlayer = VideoView(this)
+        val mediaPlayer = MediaPlayer.create(this, R.raw.funny_videos)
+        val surfaceHolder = videoPlayer.holder
+        surfaceHolder.addCallback(object : SurfaceHolder.Callback {
+
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                videoPlayer.background =
+                    ContextCompat.getDrawable(applicationContext, android.R.color.transparent)
+                mediaPlayer.setDisplay(holder)
+                mediaPlayer.start()
+            }
+
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+
+            }
+
+        })
+        mediaPlayer.setOnCompletionListener {
+            fileLoggerTimer.cancel()
+            frameLayout.removeView(videoPlayer)
+            val simpleButton = Button(this)
+            simpleButton.text = "my father my fathermy father "
+            simpleButton.text =
+                SafeAndroidUtilities.ellipsizeCenterEnd(
+                    simpleButton,
+                    "my father", 100, 10
+                )
+            frameLayout.addView(simpleButton)
+        }
+
+        frameLayout.addView(videoPlayer)
     }
 }
