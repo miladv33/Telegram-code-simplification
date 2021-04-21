e44a500cc1a0e647e0c0a0e35576f19647faddb2
m.varvaei <m@123456>
2021-04-21 22:06:37 +0430

    How telegram add a view to screen without using XML

--- app/src/main/java/com/example/simpletelegramcode/MainActivity.kt
+++ app/src/main/java/com/example/simpletelegramcode/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.simpletelegramcode
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var frameLayout:FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        frameLayout = FrameLayout(this)
+        setContentView(
+            frameLayout,
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        )
+        val simpleButton = Button(this)
+        simpleButton.text = "Button"
+        frameLayout.addView(simpleButton)
     }
 }
