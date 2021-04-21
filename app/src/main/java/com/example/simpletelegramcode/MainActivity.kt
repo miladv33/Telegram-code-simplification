31e7afe394cc375ab14cfc18d56f0fc67edd9dfd
m.varvaei <m@123456>
2021-04-22 03:01:56 +0430

    test FileLogger: it is working

--- app/src/main/java/com/example/simpletelegramcode/MainActivity.kt
+++ app/src/main/java/com/example/simpletelegramcode/MainActivity.kt
@@ -5,7 +5,11 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.example.simpletelegramcode.utils.FileLog
 import com.example.simpletelegramcode.utils.SafeAndroidUtilities
+import com.example.simpletelegramcode.utils.getMemoryPermission
+import com.example.simpletelegramcode.utils.hasMemoryPermission
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,5 +31,20 @@ class MainActivity : AppCompatActivity() {
             SafeAndroidUtilities.ellipsizeCenterEnd(simpleButton,
                 "my father",100,10)
         frameLayout.addView(simpleButton)
+        if (hasMemoryPermission()) {
+            testFileLogger()
+        } else {
+            getMemoryPermission()
+        }
+    }
+
+    private fun testFileLogger(){
+        FileLog.ensureInitied()
+        val timer = Timer()
+        timer.schedule(object :TimerTask(){
+            override fun run() {
+                FileLog.e("this is test log")
+            }
+        },2000,500)
     }
 }
